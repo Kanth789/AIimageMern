8f2ce4e95f3c94f40b60a26a86c6dbbbb75fedcd
Kiran Kanth Reddy <kanthkiran020@gmail.com>
2024-10-14 10:33:43 +0530

    add env

--- server/controllers/GenerateImages.js
+++ server/controllers/GenerateImages.js
@@ -1,10 +1,9 @@
 import * as dotenv from "dotenv";
 import { createError } from "../error.js";
-import OpenAI from "openai";
 
 dotenv.config();
 
-
+const token = process.env.HUGGING_FACE_TOKEN
 const generateImage = async (req, res, next) => {
     try {
         const { prompt } = req.body;
@@ -13,19 +12,19 @@ const generateImage = async (req, res, next) => {
             "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
             {
                 headers: {
-                    Authorization: "Bearer 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",
+                    Authorization: `Bearer ${token}`,
                     "Content-Type": "application/json",
                 },
                 method: "POST",
-                body: JSON.stringify({ inputs: prompt }), // Ensure the prompt is sent properly
+                body: JSON.stringify({ inputs: prompt }),
             }
         );
 
-        const arrayBuffer = await response.arrayBuffer(); // Get the binary data as ArrayBuffer
-        const buffer = Buffer.from(arrayBuffer); // Convert ArrayBuffer to Buffer
-        const base64Image = buffer.toString('base64'); // Convert Buffer to base64 string
+        const arrayBuffer = await response.arrayBuffer(); 
+        const buffer = Buffer.from(arrayBuffer); 
+        const base64Image = buffer.toString('base64'); 
 
-        return res.status(200).json({ photo: base64Image }); // Return the base64 string
+        return res.status(200).json({ photo: base64Image }); 
     } catch (error) {
         next(
             createError(
